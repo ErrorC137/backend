b23b70caaf3cf24121e20927bd0def4663b078cf
ErrorC137 <154239085+ErrorC137@users.noreply.github.com>
2026-06-23 15:17:01 +0700

    3333

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-PYTHONPATH=. uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+export PYTHONPATH=.
+uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
